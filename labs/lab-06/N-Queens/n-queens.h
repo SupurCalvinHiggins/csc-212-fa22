913f08487cdcd247658782d9a5d22c900dc12449
Christian Esteves <cesteves@uri.edu>
2022-10-12 12:26:57 -0400

    Update n-queens.h

--- labs/lab-06/N-Queens/n-queens.h
+++ labs/lab-06/N-Queens/n-queens.h
@@ -2,16 +2,17 @@
 #include <iostream>
 
 class NQueens{
-    private:
-        unsigned int board_size;
-        std::vector<std::vector<unsigned int> > * board;
-        bool solve_rec(int col);
-        bool solve_rec_col(int row);
-        bool is_valid(unsigned int row, unsigned int col);
-    public:
-        NQueens();
-        NQueens(unsigned int n);
-        ~NQueens();
-        void display_board();
-        bool solve();
+private:
+    unsigned int board_size;
+    std::vector<std::vector<unsigned int> > * board;
+    bool solve_rec(int col);
+    bool solve_rec_col(int row);
+    bool is_valid(unsigned int row, unsigned int col);
+    bool is_valid_col(unsigned int row, unsigned int col);
+public:
+    NQueens();
+    NQueens(unsigned int n);
+    ~NQueens();
+    void display_board();
+    bool solve();
 };
